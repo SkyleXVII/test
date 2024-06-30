33bd5bafb2304caf383a00606327dfa41afac282
Артем <113039276+SkyleXVII@users.noreply.github.com>
2024-06-30 14:28:49 +0300

    Update labinf1-2-var-reshenia.sh

--- labinf1-2-var-reshenia.sh
+++ labinf1-2-var-reshenia.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 mkdir test{1,2}
-read -p "Введите URL репозитория: " answer
+read -p "Введите URL вашего репозитория: " answer
 cd test1
 git clone $answer
 download="$(ls -tr | tail)"
 mv $download ../test2
 rm -rf ../test1
 sudo chmod 600 -R ../test2
-echo "Лабораторная работа 1 выполнена" 
+echo "Экзамен выполнен" 
